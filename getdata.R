1aa0411489669d4d8080f65422b24c62beecd1db
mjmoon <michael.jongho.moon@gmail.com>
2016-08-05 18:16:14 -0400

    Data parsed
    + data parsed for modelling and validation (data/parsedData.Rdata)

--- getdata.R
+++ getdata.R
@@ -1,13 +1,88 @@
 library(retrosheet)
 library(plyr)
-getFileNames()
+library(reshape2)
+yrs <- c(2010:2015)
+teamIds <- unique(c(sapply(yrs, getTeamIDs)))
+# Florida Marlins changed to Miami Marlins
+teamIds <- teamIds[teamIds != "FLO"]
+parkIds <- c(getParkIDs())
 
-teamIds <- getTeamIDs(2015)
+# Variables: Teams, Scores, Date; DblHdr; DayNight; ParkID; 
+fields <- c("HmTm", "VisTm", "HmLine", "VisLine", "Date", "DblHdr", "DayNight", "ParkID")
+dataGame <- lapply(yrs, function(x) getPartialGamelog(x, fields)) 
+dataGameY <- lapply(dataGame, as.data.frame)
 
-data15 <- sapply(teamIds, function(x) getRetrosheet('play', 2015, x)) 
-dataGame <- getRetrosheet('game', 2015)
-dataRoster <- getRetrosheet('roster', 2015)
-dataRoster$TOR
-head(dataGame[dataGame$DblHdr > 0,2])
-names(dataGame)
-# Date; DblHdr; DayNight; ParkID; 
+parseSeason <- function(season){
+  season$GID <- paste(paste(season$Date, season$ParkID, sep = "-"), season$DblHdr, sep = "")
+  season$Date <- as.Date(season$Date, "%Y%m%d")
+  season$DblHdr <- factor(season$DblHdr, c(0, 1, 2, 3), 
+                          c("Single", "First", "Second"))
+  # Florida Marlins changed to Miami Marlins
+  season$VisTm[season$VisTm == "FLO"] <- "MIA"
+  season$VisTm <- factor(season$VisTm, teamIds)
+  season$HmTm[season$HmTm == "FLO"] <- "MIA"
+  season$HmTm <- factor(season$HmTm, teamIds)
+  season$DayNight <- factor(season$DayNight, c("D", "N"))
+  season$ParkID <- factor(season$ParkID, parkIds$PARKID)
+  season$ScoreDiffHm <- 
+    sapply(strsplit(season$HmLine, ""), 
+           function(x) sum(as.numeric(x), na.rm = TRUE)) -
+    sapply(strsplit(season$VisLine, ""), 
+           function(x) sum(as.numeric(x), na.rm = TRUE))
+  
+  tmpVis <- subset(season, select = c("GID", "Date", "DblHdr", "VisTm", "DayNight",
+                                      "ParkID", "ScoreDiffHm"))
+  names(tmpVis) <- c("GID", "Date", "DblHdr", "Team", "DayNight",
+                     "ParkID", "ScoreDiff")
+  tmpVis$ScoreDiff <- -tmpVis$ScoreDiff
+  tmpVis$HmVis <- -1
+  
+  
+  tmpHm <- subset(season, select = c("GID", "Date", "DblHdr", "HmTm", "DayNight",
+                                      "ParkID", "ScoreDiffHm"))
+  names(tmpHm) <- c("GID", "Date", "DblHdr", "Team", "DayNight",
+                     "ParkID", "ScoreDiff")
+  tmpHm$HmVis <- 1
+  
+  
+  tmp <- rbind(tmpVis, tmpHm)
+  return(tmp[order(tmp$Date, tmp$ParkID), ])
+}
+
+dataSeasons <- lapply(dataGameY, parseSeason)
+names(dataSeasons) <- c("S2010", "S2011", "S2012", "S2013", "S2014", "S2015")
+
+# C data frame: match matrices by date with each row representing a single game 
+#               (home team: 1; away team: -1)
+parseMatches <- function(season){
+  tmp <- ddply(season, "Date", function(x) dcast(x, GID ~ Team, sum, value.var = "HmVis"))
+  tmp[is.na(tmp)] <- 0
+  tmp <- tmp[c("Date", teamIds, "GID")] # match the column order
+  return(tmp)
+}
+
+matchSeasons <- lapply(dataSeasons, parseMatches)
+names(matchSeasons) <- c("S2010", "S2011", "S2012", "S2013", "S2014", "S2015")
+
+head(matchSeasons$S2010)
+
+# Y data frame: score differences by date for each game
+# scoreSeasons <- lapply(dataSeasons, function(x) dcast(x, Date ~ Team, sum, value.var = "ScoreDiff"))
+scoreSeasons <- lapply(dataSeasons, function(x) subset(x, HmVis == 1, select = c("Date", "ScoreDiff", "GID")))
+names(scoreSeasons) <- c("S2010", "S2011", "S2012", "S2013", "S2014", "S2015")
+
+head(scoreSeasons$S2010)
+
+# U data frame: covariates matrices by date
+parseCovs <- function(season){
+  season$Mth <- months(season$Date)
+  row.names(season) <- NULL
+  return(subset(season, select = c("Date", "Mth", "DblHdr", "DayNight", "ParkID", "GID")))
+}
+
+covSeasons <- lapply(dataSeasons, parseCovs)
+names(covSeasons) <- c("S2010", "S2011", "S2012", "S2013", "S2014", "S2015")
+
+head(covSeasons$S2010)
+
+save(matchSeasons, scoreSeasons, covSeasons, file = "./data/parsedData.RData")
